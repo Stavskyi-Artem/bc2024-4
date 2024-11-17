08ec24ebc094c05024e1fe831f130f092a328562
Stavskyi-Artem <artemonder.artem@gmail.com>
2024-11-17 22:50:18 +0200

    Implement GET method to retrieve cached images

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Command } = require('commander');
 const http = require('http');
+const { promises: fs } = require('fs');
+const path = require('path');
 
 const program = new Command();
 
@@ -12,9 +14,38 @@ program.parse(process.argv);
 
 const options = program.opts();
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8'); // Додано заголовок Content-Type
-    res.end('Сервер працює');
+const cacheDir = options.cache;
+
+const server = http.createServer(async (req, res) => {
+    const urlParts = req.url.split('/');
+    const httpCode = urlParts[1];
+
+    if (!httpCode.match(/^\d{3}$/)) {
+        res.statusCode = 400;
+        res.end('Bad Request');
+        return;
+    }
+
+    const filePath = path.join(cacheDir, `${httpCode}.jpg`);
+
+    switch (req.method) {
+        case 'GET':
+            try {
+                const fileData = await fs.readFile(filePath);
+                res.setHeader('Content-Type', 'image/jpeg');
+                res.statusCode = 200;
+                res.end(fileData);
+            } catch (err) {
+                res.statusCode = 404;
+                res.end('Not Found');
+            }
+            break;
+
+        default:
+            res.statusCode = 405;
+            res.end('Method Not Allowed');
+            break;
+    }
 });
 
 server.listen(options.port, options.host, () => {
